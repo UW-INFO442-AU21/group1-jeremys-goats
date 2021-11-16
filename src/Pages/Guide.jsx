748c4c06f7b508608174e6e810612ae333f14ac4
Ryan Lee <ralee478.school@gmail.com>
2021-11-16 10:43:23 -0800

    data can now be read from const data in Guide.jsx

--- src/Pages/Guide.jsx
+++ src/Pages/Guide.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react';
+
 const Guide = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("csvjson.json")
+                .then(res => res.json())
+                .then(jsonData => setData(jsonData));
+    }, [])
+
+    console.log(data)
     return (
         <div>
             <p>Guide</p>
