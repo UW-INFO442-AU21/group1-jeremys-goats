fb9280c42e4bb4132a6caff7911d55aa0289d595
Dana Rin <danarin@uw.edu>
2021-11-16 15:13:32 -0800

    added cards, ordering is being weird lolz

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,6 +2,53 @@ export function Cards() {
     return (
         <div>
             {/* cards go here */}
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 1
+                        </p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 2
+                        </p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 3
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="column">
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 4
+                        </p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 5
+                        </p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <p>
+                            Article 6
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
