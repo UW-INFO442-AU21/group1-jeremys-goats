0258f9db8200895416972a7307c770cfbf3ffb31
lavishal <ajt33@uw.edu>
2021-11-26 14:50:09 -0800

    about page update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,38 @@
 const About = () => {
     return (
-        <div>
-            <p>About</p>
+<div> 
+            <div className="flex-container">
+                <section className="flex-item">
+                    <div className="About-PageTxt">
+                    <header>
+                        <h2>Problem Statement</h2>
+                    </header>
+                         <p className="problem">
+                        Data label confusion is one of many factors that contributes to the food waste problem.
+                        </p>
+                     </div>
+                   
+                    <div className="Our-SolutionTxt">
+                    <header>
+                        <h2>Our Solution</h2>
+                    </header>
+                        <p className="solution">
+                        An expiration date guide that allows our users to figure out when their food expires and mitigate the food waste problem. 
+                        </p>
+                    </div>
+                    
+                    <div className="GoalTxt">
+                    <header>
+                        <h2>Our Goal</h2>
+                    </header>
+                        <p className="goal">
+                        Our goal is to potentially reduce the amount of food that goes to waste. 
+                        </p>
+                    </div>
+            </section>
+           </div>
         </div>
-    )
+    );
 }
 
 export default About;
