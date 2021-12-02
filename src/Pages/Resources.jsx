408ca4b7bb24580086656dd4764c666f906a4a7a
Ryan Lee <ralee478.school@gmail.com>
2021-12-02 11:42:43 -0800

    done

--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -1,10 +1,10 @@
-import { cards } from "../Components/Cards"
+import { Cards } from "../Components/Cards"
 
 const Card = () => {
     return (
         <div>
             <p>Click on one of the article links below to get more tips and information about what you could do to mitigate food waste.</p>
-            
+            <Cards/>
         </div>
     )
 }
